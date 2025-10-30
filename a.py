ee2d54d103d0daeb8c5fff85d1a51098c0232f04
Tamle <tamleminhtam437@gmail.com>
2025-10-30 22:00:15 +0700

    a

--- a.py
+++ a.py
@@ -17,3 +17,79 @@ def c01():
     pass
 def a03():
     pass
+print("Hello")
+
+def add():
+    print("FUCK YOU")
+    
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
+print("Hello")
+
+def add():
+    print("FUCK YOU")
+    
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
+print("Hello")
+
+def add():
+    print("FUCK YOU")
+    
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
+print("Hello")
+
+def add():
+    print("FUCK YOU")
+    
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
