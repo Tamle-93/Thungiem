bd4f36f38d99e40f153e89d42e93fc09ffffab3e
HP <nguyenvonhatthuan@gmail.com>
2025-10-30 21:59:47 +0700

    From T

--- a.py
+++ a.py
@@ -14,3 +14,32 @@ def c01():
     pass
 def a03():
     pass
+
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
+def add(a, b):
+    return a * b - c
+def a01():
+    pass
+
+def b01():
+    print("a")
+def a02():
+    pass
+
+def c01():
+    pass
+def a03():
+    pass
